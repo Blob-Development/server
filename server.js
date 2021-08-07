87fd4646d7b6a6acf757703413867c3a0f4df6ca
ray <33544674+ray-1337@users.noreply.github.com>
2021-08-07 21:27:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require("express"),
       app = express(),
       ms = require("ms"),
       fs = require("fs"),
-      port = Number(1111),
+      port = Number(1111), // you can change this
       helmet = require("helmet"),
       compression = require('compression');
 
@@ -22,7 +22,6 @@ let locale = __dirname + "/files", // file that will be served into public.
     assets = __dirname + "/assets"; // normal assets such as index.html or else.
 
 // normal configuration
-app.disable('x-powered-by');
 app.use(helmet());
 
 // cors
@@ -31,17 +30,6 @@ app.use(cors(corsOpt));
 // compress the file, always compress it, no matter what
 app.use(compression());
 
-// i dont think this thing should be cached.
-app.use(express.static(assets));
-
-// caching
-app.use(express.static(locale, {maxAge: ms("24h")}));
-
-app.use((req, res, next) => {
-  res.setHeader('Cache-Control', 'public, max-age=86400, immutable');
-  next();
-});
-
 // usually return favicon.ico, use "res.status(204).end()" if you want to.
 app.get('/favicon.ico', (req, res) => res.status(200).sendFile(assets + "/favicon.ico"));
 
@@ -49,29 +37,10 @@ app.get('/favicon.ico', (req, res) => res.status(200).sendFile(assets + "/favico
 app.get('/robots.txt', (req, res) => res.sendFile(assets + "/robots.txt"));
 
 // example: https://cdn.blob-project.com/
-app.get("/", (req, res) => res.status(200).send("ok"));
+app.get("/", (req, res) => res.status(200).send("OK"));
 
-// some sort of error handling
-app.use("/files", (req, res, next) => {
-  if (fs.existsSync(locale + req.url)) { // if the file exists, skip.
-    next();
-  } else { // return if its not found.
-    res.status(404).send("unknown resource.");
-    next();
-  };
-});
-
-// example: https://cdn.blob-project.com/files/to/your/things
-app.get("/files", (req, res, next) => {
-  res.sendFile(locale + req.url, (err) => { // send files
-    if (err) {
-      res.status(404).send("unknown resource.");
-      next();
-    } else {
-      next();
-    }
-  })
-});
+// Router/Files
+app.use('/files', require('./router/files'));
 
 // ready to be served.
 app.listen(port, () => console.log(`Started on PORT: ${port}`)); // PORT = 1111 (default, you can change it)
